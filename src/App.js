78ea5043ca2f29ff91154790bfd0838da2e621e0
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-09 13:11:39 -0400

    Add routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Home from './containers/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import NavBar from './components/NavBar';
+// import Subjects from './components/Subjects';
+// import SubTopics from './components/SubTopics';
+// import FlashCards from './components/FlashCards';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2> THE STUDY </h2>
+      </div>
+      <Router>
+        <div className="App-routes">
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/*<Route exact path="/subjects" component={Subjects} />
+             <Route exact path="/sub_topics" component={SubTopics} />
+            <Route exact path="/flash_cards" component={FlashCards} /> */}
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+   
+  )
 }
 
 export default App;
