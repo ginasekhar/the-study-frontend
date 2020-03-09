80ea932f2cf68323964dc069d1fad36a1abf7a51
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-09 13:16:05 -0400

    Create skeleton homepage

---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="Home-page">
+        <p> This is the home page</p> 
+      </div>
+    )
+  }
+}
+
+export default Home
