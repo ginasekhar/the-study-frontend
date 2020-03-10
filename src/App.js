cf15c6cde1ce9f975a4069d98a0c3acaa217b7d3
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-09 21:11:50 -0400

    set up routing, dynamic links happen in navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Home from './containers/Home'
 import {
@@ -7,33 +7,35 @@ import {
   Route
 } from 'react-router-dom';
 import NavBar from './components/NavBar';
-// import Subjects from './components/Subjects';
-// import SubTopics from './components/SubTopics';
-// import FlashCards from './components/FlashCards';
 import './App.css';
 import logo from './logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h2> THE STUDY </h2>
-      </div>
-      <Router>
-        <div className="App-routes">
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            {/*<Route exact path="/subjects" component={Subjects} />
-             <Route exact path="/sub_topics" component={SubTopics} />
-            <Route exact path="/flash_cards" component={FlashCards} /> */}
-          </Switch>
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2> THE STUDY </h2>
         </div>
-      </Router>
-    </div>
-   
-  )
+        <Router>
+          <div className="App-routes">
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              {/*<Route exact path="/subjects" component={Subjects} />
+               <Route exact path="/sub_topics" component={SubTopics} />
+              <Route exact path="/flash_cards" component={FlashCards} /> */}
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+  
 }
+
 
 export default App;
