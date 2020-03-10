72f8ba711e6eefcfcec33f25ac13231503f964f1
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-10 07:11:42 -0400

    Add welcome message to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,9 @@ class Home extends Component {
   render() {
     return (
       <div className="Home-page">
-        <p> This is the home page</p> 
+        <h2>Welcome to "The Study".</h2> 
+        <h3>We are proud to have a collection of flash cards on a variety of topics </h3> 
+        <h3>Please click on one of the subject buttons above in the navigation bar to view topics available in each subject </h3>
       </div>
     )
   }
