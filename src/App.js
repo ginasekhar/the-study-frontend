411aa172f166be7d26861d623d7f7f36ee31a612
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-10 14:49:52 -0400

    fixed routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import NavBar from './components/NavBar';
 import './App.css';
 import logo from './logo.png';
+import TopicsContainer from './containers/TopicsContainer'
 
 
 class App extends Component {
@@ -17,17 +18,18 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <h2>&nbsp;&nbsp;&nbsp;</h2>
           <img src={logo} className="App-logo" alt="logo"/>
-          <h2> THE STUDY </h2>
+          <h2> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; THE STUDY </h2>
         </div>
         <Router>
           <div className="App-routes">
             <NavBar />
             <Switch>
               <Route exact path="/" component={Home} />
-              {/*<Route exact path="/subjects" component={Subjects} />
-               <Route exact path="/sub_topics" component={SubTopics} />
-              <Route exact path="/flash_cards" component={FlashCards} /> */}
+              <Route exact path="/subjects/:id/sub_topics" component={TopicsContainer} />
+              {/* <Route exact path="/topics/:id/flash_cards" component={FlashCardsContainer} />
+              <Route exact path="/flash_card/:id" component={FlashCards} /> */}
             </Switch>
           </div>
         </Router>
