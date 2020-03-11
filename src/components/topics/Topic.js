0886ec8fc1a3abb545c85eb430ecd69aba480625
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-11 16:38:21 -0400

    clean up commented code

--- src/components/topics/Topic.js
+++ src/components/topics/Topic.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { deleteTopic } from '../../actions/topics'
+
+import { addFlashCard, getFlashCards} from '../../actions/flashcards'
+import { deleteTopic} from '../../actions/topics'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 import './Topic.css' 
 
 
@@ -11,15 +14,15 @@ class Topic extends Component {
     this.props.deleteTopic(this.props.topic.id)
   } 
 
-  // handleOnClickAddFC = (event) => { 
-  //   console.log("just before call to addFC in Topic.js", this.props)
-  //   this.props.addFlashCard(this.props.topic.id)
-  // } 
+  handleOnClickAddFC = (event) => { 
+    console.log("just before call to addFC in Topic.js", this.props)
+    this.props.addFlashCard(this.props.topic.id)
+  } 
 
-  // handleOnClickViewFC = (event) => { 
-  //   console.log("just before call to addFC in Topic.js", this.props)
-  //   this.props.viewFlashCards(this.props.topic.id)
-  // } 
+  handleOnClickViewFC = (event) => { 
+    console.log("just before call to getFC in Topic.js", this.props)
+    this.props.getFlashCards(this.props.topic.id)
+  } 
 
   render() {
     const { topic } = this.props;
@@ -27,21 +30,31 @@ class Topic extends Component {
     return (
       <div className="card">
         <li>
-          <strong>{topic.name}</strong><br/><br/>{topic.description}
-          <button id={topic.id} className="delete" onClick={this.handleOnClickDelete}> X </button>
-          {/* <button id={topic.id} href="" className="add-flash-card" onClick={this.handleOnClickAddFC}> AddFC </button>
-          <button id={topic.id} href="" className="view-flash-cards" onClick={this.handleOnClickViewFC}> AddFC </button> */}
-          
+          <strong>{topic.name}</strong><br/><br/>{topic.description}<br/>
+
+          <button topic-id={topic.id} className="delete" onClick={this.handleOnClickDelete}> X </button>
+
+          <Link className="view-fc-link" 
+          to={`/sub_topics/${topic.id}/flash_cards`} 
+          key={topic.id}> 
+            <button type="button" 
+              topic-id={topic.id} onClick={this.handleOnClickViewFC}>
+              View FlashCards
+            </button>
+        </Link>  
+        <Link className="add-flash-card" 
+          to={`/sub_topics/${topic.id}/flash_cards/new`} 
+          key={topic.id}> 
+            <button type="button" 
+              topic-id={topic.id} onClick={this.handleOnClickAddFC}>
+              Add a FlashCard
+            </button>
+        </Link>  
         </li>
       </div>
     );
   }
 };
 
-const mapDispatchToProps = dispatch => ({
-  deleteTopic: id => dispatch({ type: "DELETE_TOPIC", id })
-  // ,addFlashCard: flashCard => dispatch({ type: "ADD_FLASHCARD", flashCard})
-  // ,deleteTopic: topicId => dispatch({ type: "VIEW_FLASHCARDS", topicId })
-})
+export default connect (null, {deleteTopic, getFlashCards, addFlashCard}) (Topic);
 
-export default connect (null, mapDispatchToProps) (Topic);
