7997d64c88e1d745a69e90a1893025cb882c63a6
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-13 14:27:24 -0400

    formatting

--- src/components/flashCards/FlashCards.js
+++ src/components/flashCards/FlashCards.js
@@ -7,16 +7,19 @@ class FlashCards extends Component {
     
     console.log("In FlashCards, I got passed in", this.props.flashcards)
     const myflashCards =  this.props.flashCards.map(flashCard => 
-    
-    <FlashCard 
-      key={flashCard.id}
-      flashcard={flashCard} />)
-    return (
-      <ul>
-        {myflashCards.length > 0 ? myflashCards : "No flashcards yet"}
-      </ul>
-    );
-  }
+       <FlashCard key={flashCard.id} flashcard={flashCard} />
+    )
+      
+        return (
+        <div className="flash-cards-list flex-column flex-wrap">
+          <h3> Flash Cards for {this.props.topicName} </h3>
+          <ul className="flash-cards-ul">
+            {myflashCards.length > 0 ? myflashCards : "No flashcards yet"}
+          </ul>
+        </div>
+      );
+
+    }
 };
 
 export default FlashCards;
