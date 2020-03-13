2e296717b10d5d6c3c9df9715bb47d4dc3ceca9a
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-13 14:26:06 -0400

    change formatting

--- src/components/topics/TopicInput.js
+++ src/components/topics/TopicInput.js
@@ -21,42 +21,49 @@ class TopicInput extends Component {
 
     const topic = {...this.state, subject_id: this.props.subjectId}
     
-    console.log ("going to add topic", topic)
     this.props.addTopic(topic);
     
-    this.setState({
+    this.setState({...this.state,
       name: '',
-      description: '',
-      subject_id: ''
+      description: ''
+      // don't reset subject id
+
     });
   }
   render() {
     return (
-      <div className="topic-input-form">
+      <div className="topic-input-container">
+        <strong>Add a New Topic </strong> 
         <form onSubmit={(event) => this.handleOnSubmit(event)}>
-        <label>
-             <h4>Add a New Topic </h4> <br/>
-            Enter Topic Name:
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={(event) => this.handleOnChange(event)} />
-          </label> <br/>
-          <label>
-            Enter Topic Description:
-          <input
-            type="text"
-            name="description"
-            value={this.state.description}
-            onChange={(event) => this.handleOnChange(event)} />
-          </label> <br/>
-          <input type="submit" className="submit-button" value="Add Topic" />
+          <div class="form-group form-control-sm">
+            <div class="input-group input-group-sm">
+              <div class="input-group-prepend">
+                <span class="input-group-text input-group-sm">Topic Name</span>
+              </div>
+              <input class="form-control" placeholder="Topic Name"
+                type="text" name="name" value={this.state.name}
+                onChange={(event) => this.handleOnChange(event)} 
+              />
+            </div>
+            <div class="form-group form-control-sm">
+              <div class="input-group input-group-sm">
+                <div class="input-group-prepend">
+                  <span class="input-group-text input-group-sm">Description</span>
+                </div>
+                <input class="form-control" placeholder="Topic Description"
+                  type="text" name="description" value={this.state.description}
+                  onChange={(event) => this.handleOnChange(event)} 
+                />
+              </div>
+            </div>
+            <input type="submit" className="submit-button btn-sm" value="Add Topic" /> 
+          </div>
         </form>
       </div>
     );
   }
 };
 
-
 export default connect (null, {addTopic}) (TopicInput);
+
+{/* <button type="submit" class="btn btn-sm">Add Topic</button> */}
