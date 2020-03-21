4e9484de2994e35ab6e3afd3d71908a8df853148
Gina Sekhar <ginasekhar@yahoo.com>
2020-03-21 13:35:52 -0400

    Add sort and filter functionality

--- src/containers/FlashCardsContainer.js
+++ src/containers/FlashCardsContainer.js
@@ -6,6 +6,56 @@ import { connect } from 'react-redux'
 
 class FlashCardsContainer extends Component {
 
+  state = {
+    sortOrder: "none",
+    searchTerm: ""
+  }
+
+  handleOnChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name] : value,
+    });
+  }
+
+  sortAndFilterFlashCards = (list) => {
+
+    let filteredList = list;
+
+    if (this.state.searchTerm.trim() !== "") {
+      const ucaseSearchTerm = this.state.searchTerm.toUpperCase()
+      
+      filteredList = list.filter(flashcard => {
+        return (flashcard.question.toUpperCase().includes(ucaseSearchTerm) ||
+                flashcard.answer.toUpperCase().includes(ucaseSearchTerm))
+      })
+      
+      console.log("filtered list", filteredList)
+
+    } 
+    
+    switch(this.state.sortOrder) {
+      case "none" :
+        break;
+      case "ques-a" : 
+      filteredList.sort((a, b) => (a.question.toUpperCase() > b.question.toUpperCase()) ? 1 : -1)
+        break;
+      case "ques-d" : 
+      filteredList.sort((a, b) => (b.question.toUpperCase() > a.question.toUpperCase()) ? 1 : -1)
+        break;
+      case "ans-a" : 
+      filteredList.sort((a, b) => (a.answer.toUpperCase() > b.answer.toUpperCase()) ? 1 : -1)
+        break; 
+      case "ans-d" : 
+      filteredList.sort((a, b) => (b.answer.toUpperCase() > a.answer.toUpperCase()) ? 1 : -1)      
+      default :
+        break;
+    } 
+      
+    return filteredList;
+
+  }
+
   render() {
     
     const topicId = this.props.currentTopic.topicId
@@ -19,7 +69,26 @@ class FlashCardsContainer extends Component {
     return (
       <div>
         <FlashCardInput topicId={topicId}/> 
-        <FlashCards flashCards={flashCardsList} topicName={topicName}/>
+        <div className='sort-selection'>
+          <label>
+            Choose a sort order:
+          <select value={this.state.sortOrder} name="sortOrder" id="sort-order" onChange={this.handleOnChange}>
+            <option value="none">None</option>
+            <option value="ques-a">Term Ascending</option>
+            <option value="ques-d">Term Descending</option>
+            <option value="ans-a">Definition Ascending</option>
+            <option value="ans-d">Definition Descending</option>
+          </select>
+          </label>
+        </div>  
+        <div>
+          <label>
+            Filter results by keyword:
+            <input name="searchTerm" type="text" className="search-term" placeholder="Keyword" onChange={this.handleOnChange}/>
+          </label>
+        </div>
+
+        <FlashCards flashCards={this.sortAndFilterFlashCards(flashCardsList)} topicName={topicName}/>
       </div>
     )
   }
--- src/containers/TopicsContainer.js
+++ src/containers/TopicsContainer.js
@@ -6,16 +6,91 @@ import { connect } from 'react-redux'
 
 
 class TopicsContainer extends Component {
+
+  state = {
+    sortOrder: "none",
+    searchTerm: ""
+  }
+
+  handleOnChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name] : value,
+    });
+  }
+  
+  sortAndFilterTopics = (list) => {
+
+    let filteredList = list;
+
+    if (this.state.searchTerm.trim() !== "") {
+      const ucaseSearchTerm = this.state.searchTerm.toUpperCase()
+      
+      filteredList = list.filter(topic => {
+        return (topic.description.toUpperCase().includes(ucaseSearchTerm) ||
+                topic.name.toUpperCase().includes(ucaseSearchTerm))
+      })
+      
+      // console.log("filtered list", filteredList)
+
+    } 
+    
+    switch(this.state.sortOrder) {
+      case "none" :
+        break;
+      case "name-a" : 
+      filteredList.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+        break;
+      case "name-d" : 
+      filteredList.sort((a, b) => (b.name.toUpperCase() > a.name.toUpperCase()) ? 1 : -1)
+        break;
+      case "dscr-a" : 
+      filteredList.sort((a, b) => (a.description.toUpperCase() > b.description.toUpperCase()) ? 1 : -1)
+        break; 
+      case "dscr-d" : 
+      filteredList.sort((a, b) => (b.description.toUpperCase() > a.description.toUpperCase()) ? 1 : -1)      
+      default :
+        break;
+    } 
+      
+    return filteredList;
+
+  }
+
+
   render() {
     let topicsList = [];
 
+    
     if (this.props.currentSubject.selected )
        topicsList = this.props.topics.filter(topic => topic.subject_id == this.props.currentSubject.subjectId )
     
     return (
       <div className="topics-container">
         <TopicInput subjectId={this.props.currentSubject.subjectId}/>
-        <Topics subjectName={this.props.currentSubject.subjectName} topics={topicsList}/>
+
+        <div className='sort-selection'>
+          <label>
+            Choose a sort order:
+          <select value={this.state.sortOrder} name="sortOrder" id="sort-order" onChange={this.handleOnChange}>
+            <option value="none">None</option>
+            <option value="name-a">Topic Name Ascending</option>
+            <option value="name-d">Topic Name Descending</option>
+            <option value="dscr-a">Topic Description Ascending</option>
+            <option value="dscr-d">Topic Description Descending</option>
+          </select>
+          </label>
+        </div>  
+        <div>
+          <label>
+            Filter results by keyword:
+            <input name="searchTerm" type="text" className="search-term" placeholder="Keyword" onChange={this.handleOnChange}/>
+          </label>
+        </div>
+
+        
+        <Topics subjectName={this.props.currentSubject.subjectName} topics={this.sortAndFilterTopics(topicsList)}/>
+        
       </div>
     )
   }  
